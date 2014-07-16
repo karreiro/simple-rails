a01155bc4a8d98b6c78be33925be4c1abeecfa4e
Guilherme Carreiro <karreiro@gmail.com>
2014-07-16 10:44:17 -0300

    simple rails

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple Rails
+===
+
+Exemplo utilizado no curso de Ruby ministrado por mim na [Dextraining](http://www.dextraining.com.br/).
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'Guilherme', email: 'guilherme.silve@dextra.com.br')
+User.create(name: 'Paulo', email: 'paulo.silva@dextra.com.br')
+User.create(name: 'Tiago', email: 'tiago.sousa@dextra.com.br')
+User.create(name: 'Evellyn', email: 'evellyn.sousa@dextra.com.br')
